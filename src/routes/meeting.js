b2914863873048e6f83322914a9854ec55e293d9
gamador <gamadorh93@gmail.com>
2020-02-22 16:40:52 -0600

    auth was added en preValidation

--- src/routes/meeting.js
+++ src/routes/meeting.js
@@ -4,26 +4,31 @@ const sch = require('../schemes/meeting')
 module.exports = (app, _options, next) => {
   // add one
   app.post('/', {
+    preValidation: app.auth,
     schema: sch.addOne
   }, ctl.addOne)
 
   // get all
   app.get('/', {
+    preValidation: app.auth,
     schema: sch.getAll
   }, ctl.getAll)
 
   // get one
   app.get('/:id', {
+    preValidation: app.auth,
     schema: sch.getOne
   }, ctl.getOne)
 
   // update one
   app.put('/:id', {
+    preValidation: app.auth,
     schema: sch.updateOne
   }, ctl.updateOne)
 
   // delete one
   app.delete('/:id', {
+    preValidation: app.auth,
     schema: sch.deleteOne
   }, ctl.deleteOne)
 
