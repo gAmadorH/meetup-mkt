bb9857bf1456a7987ea6e7c1bf0ff4e4339dd994
gamador <gamadorh93@gmail.com>
2020-02-23 20:34:02 -0600

    src/app.js was refacted

--- src/app.js
+++ src/app.js
@@ -10,30 +10,40 @@ const {
 
 const app = fastify({ logger })
 
+// ecosystem plugins
 app.register(require('fastify-chalk'))
 app.register(require('fastify-graceful-shutdown'))
+app.register(require('fastify-jwt'), { secret: jwtSecret })
 app.register(require('fastify-sensible'))
-app.register(require('fastify-jwt'), {
-  secret: jwtSecret
-})
-
-app.register(require('./auth'), { database })
-app.register(require('./sequelize'), { database })
-app.register(require('./models-loader'))
-app.register(require('./routes'), { prefix: '/api' })
 
 app.after((err) => {
-  app.log.info('NODE_ENV\t\t[%s]', app.chalk.magenta(environment))
-
   if (err) {
-    app.log.error('CUSTOM PLUGINS\t[%s]', app.chalk.red('ERROR'))
+    app.log.error('ECOSYSTEM PLUGINS\t[%s]', app.chalk.red('error'))
     throw err
   }
 
+  app.log.info('ECOSYSTEM PLUGINS\t[%s]', app.chalk.magenta('ready'))
+  app.log.info('ENVIRONMENT\t\t[%s]', app.chalk.magenta(environment))
+})
+
+// system plugins
+app.register(require('./auth'))
+app.register(require('./sequelize'), { database })
+app.register(require('./models-loader'))
+app.register(require('./routes'))
+
+app.after((err) => {
+  if (err) {
+    app.log.error('SYSTEM PLUGINS\t[%s]', app.chalk.red('error'))
+    fastify.log.error(err)
+  }
+
   app.gracefulShutdown((signal, done) => {
-    app.log.info('SIGNAL\t[%s]', app.chalk.white(signal))
+    app.log.info('SIGNAL\t\t[%s]', app.chalk.magenta(signal))
     done()
   })
+
+  app.log.info('SYSTEM PLUGINS\t[%s]', app.chalk.magenta('ready'))
 })
 
 module.exports = app
