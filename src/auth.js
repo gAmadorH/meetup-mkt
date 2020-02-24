9ca26fd1675a873a4eca727cbb44e86b4aa5d069
gamador <gamadorh93@gmail.com>
2020-02-23 20:35:13 -0600

    src/auth.js was refacted

--- src/auth.js
+++ src/auth.js
@@ -1,20 +1,26 @@
 const fp = require('fastify-plugin')
 
-function fastifyAuth(fastify, _options, done) {
-  fastify.decorate('auth', function auth(request, _reply, next) {
-    request.jwtVerify((err, decoded) => {
-      if (err) {
-        next(this.httpErrors.unauthorized(err.message))
-      } else {
-        request.auth.user = decoded.user
-        next()
-      }
-    })
+function auth(request, _reply, done) {
+  request.jwtVerify((err, decoded) => {
+    if (err) {
+      done(this.httpErrors.unauthorized(err.message))
+    } else {
+      request.auth.user = decoded.user
+      done()
+    }
   })
+}
 
+function pg(app, _options, done) {
+  app.decorate('auth', auth)
+
+  app.log.info('AUTH\t\t\t[%s]', app.chalk.magenta('ready'))
   done()
 }
 
-module.exports = fp(fastifyAuth, {
-  fastify: '>=0.13.1'
+module.exports = fp(pg, {
+  fastify: '>=0.13.1',
+  decorators: {
+    fastify: ['chalk']
+  }
 })
