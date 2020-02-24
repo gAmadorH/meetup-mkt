86f6f9fb0f1c425d80471f24b74cc19de5329bd1
gamador <gamadorh93@gmail.com>
2020-02-23 20:31:12 -0600

    src/index.js was refacto

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const app = require('./app')
 
 const { port, host } = config
 
-app.listen(port, host).then(() => {
-  app.log.info('SERVER\t\t[%s]', app.chalk.magenta('READY'))
-  app.log.info('HOST\t\t\t[%s]', app.chalk.magenta(port))
-  app.log.info('PORT\t\t\t[%s]', app.chalk.magenta(host))
+app.listen(port, host).then((address) => {
+  app.log.info('SERVER\t\t[%s]', app.chalk.magenta('ready'))
+  app.log.info('ADDRESS\t\t[%s]', app.chalk.magenta(address))
 
   app.log.debug(app.chalk.white('\n'))
   app.log.debug(app.chalk.white(app.printRoutes()))
+}).catch((err) => {
+  app.log.error('SERVER\t\t[%s]', app.chalk.red('error'))
+  throw err
 })
